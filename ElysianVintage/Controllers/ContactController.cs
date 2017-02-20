90a10b68e86ccbc67a2027eed276ade081db192a
Dan <sheerscorpion@gmail.com>
2017-02-19 20:07:36 -0600

    built out mailing list form and more content on Contact page

---
+++ ElysianVintage/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using ElysianVintage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElysianVintage.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(MailingListingModel message)
+        {
+            string emailAddress = message.emailAddress;
+            bool mailingList = message.mailingList;
+
+            return View();
+        }
+        
+    }
+}
---
+++ ElysianVintage/Models/MailingListingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElysianVintage.Models
+{
+    public class MailingListingModel
+    {
+        [Required]
+        public string emailAddress { set; get; }
+
+        [Required]
+        public bool mailingList { set; get; }
+    }
+}
